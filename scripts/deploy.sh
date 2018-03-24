bc884eea3203064774a135b8079e66df321419e5
Felipe Quintanilla <fquintan.m@gmail.com>
2018-03-23 22:46:43 -0300

    Fixed typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ if [ $TRAVIS_BRANCH == 'master' ] ; then
     mv deploy_key ~/.ssh/id_rsa
     openssl aes-256-cbc -K $encrypted_a79c3ded4ca6_key -iv $encrypted_a79c3ded4ca6_iv -in host_key.enc -out host_key -d
     rm host_key.enc
-    cat host_key >> ~/.ssh/id_rsa
+    cat host_key >> ~/.ssh/known_hosts
 
     git init
         
